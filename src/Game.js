ca79648887dfb68e554f354782a6f2d4a63721d5
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-06-22 15:07:29 -0500

    added more logic

--- src/Game.js
+++ src/Game.js
@@ -1,20 +1,48 @@
 import { useState } from "react";
 
-//component to make a square
-const Square = ({ value, onClickSquare }) => {
+export const Square = ({ value, onSquareClick }) => {
   return (
-    <button className="square" onClick={onClickSquare}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 };
 
-//component to create the board
-const Board = ({xIsNext, squares, onPlay}) => {
-    const handleClick = (i) => {
-        
+//component to make the board
+export const Board = () => {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const handleClick = (i) => {
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
     }
+    setSquares(nextSquares);
+  };
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+};
 
-}
-
-export default Square;
+//component to create the board
+// const Board = ({ xIsNext, squares, onPlay }) => {
+//   const handleClick = (i) => {};
+// };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Game from "./Game";
+import { Board } from "./Game";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <Game />
+
+    {/* <Rows /> */}
+    <Board />
   </React.StrictMode>
 );
 
