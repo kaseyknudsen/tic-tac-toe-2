5e94fe07a6d94517d7ac81411fd804529683fb11
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-06-24 20:33:43 -0700

    moved CalculateWinner and Square to components folder

--- src/Game.js
+++ src/Game.js
@@ -1,22 +1,14 @@
 import { useState } from "react";
-
+import Square from "./components/Square";
+import CalculateWinner from "./components/CalculateWinner";
 export const Title = () => {
-    return (
-        <div className="title">Let's Play Tic Tac Toe!</div>
-    )
-}
-const Square = ({ value, onSquareClick }) => {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
+  return <div className="title">Let's Play Tic Tac Toe!</div>;
 };
 
 //component to make the board
 const Board = ({ xIsNext, squares, onPlay }) => {
   const handleClick = (i) => {
-    if (squares[i] || calculateWinner(squares)) {
+    if (squares[i] || CalculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -28,7 +20,7 @@ const Board = ({ xIsNext, squares, onPlay }) => {
     }
     onPlay(nextSquares);
   };
-  const winner = calculateWinner(squares);
+  const winner = CalculateWinner(squares);
   let status;
   if (winner) {
     status = "Winner is: " + winner;
@@ -83,7 +75,9 @@ export const Game = () => {
     }
     return (
       <li key={move}>
-        <button className="button" onClick={() => jumpTo(move)}>{description}</button>
+        <button className="button" onClick={() => jumpTo(move)}>
+          {description}
+        </button>
       </li>
     );
   });
@@ -97,24 +91,4 @@ export const Game = () => {
       </div>
     </div>
   );
-};
-
-const calculateWinner = (squares) => {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 };
